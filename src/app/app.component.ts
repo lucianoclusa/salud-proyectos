a6c45fea5f766a3ff90cd2c1a215fad7c821f727
jgalloni <jgalloni91@gmail.com>
2016-10-01 15:22:23 -0300

    cambios de test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  proys = ['PNOCH01','QOSDE05','QOSDE11','QOSDE03'];
 }
